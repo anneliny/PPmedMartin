b8c21ff90c20b3d241f75cf095b70371f607a14c
anneliny <157688768+anneliny@users.noreply.github.com>
2024-06-19 12:29:59 +0200

    Initial commit

---
+++ ShoppingCenter.cs
@@ -0,0 +1,40 @@
+﻿
+namespace PPmedMartin
+{
+    internal class ShoppingCenter
+    {
+        public List<Stores> StoresInCenter { get; private set; }
+        public ShoppingCenter()
+        {
+
+            Main();
+        }
+
+        public void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Nå skal det shoppes! Hva vil du gjøre?");
+                Console.WriteLine("1. Se alle butikker"); //Velge å sortere etter pris
+                Console.WriteLine("2. Legg til butikk");
+                Console.WriteLine("3. Avslutt");
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        var store = new Stores();
+                        store.ShowStores();
+                        break;
+                    case "2":
+                        var addStores = new Stores();
+                        addStores.AddStore();
+                        break;
+                    default:
+                        System.Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Stores.cs
@@ -0,0 +1,59 @@
+﻿
+namespace PPmedMartin
+{
+    internal class Stores
+    {
+        public string Name { get; private set; }
+        public string Speciality { get; private set; }
+        public int PriceRange { get; private set; }
+
+        public List<Stores> StoresInCenter {  get; private set; }
+
+        public Stores(string name, string speciality, int priceRange)
+        {
+            Name = name;
+            Speciality = speciality;
+            PriceRange = priceRange;
+        }
+        public Stores()
+        {
+            StoresInCenter = new List<Stores>()
+            {
+                new Stores("Hm", "Klær", 2),
+                new Stores("Panduro", "Hobby", 2),
+                new Stores("Bruktbutikk", "Diverse", 1),
+                new Stores("Chanel", "Sminke", 3),
+                new Stores("Normal", "Diverse", 1),
+                new Stores("Bolia", "Interiør", 3),
+
+            };
+        }
+
+        public void ShowStores()
+        {
+            foreach (Stores shops in StoresInCenter)
+            {
+                Console.WriteLine($"Navn: {shops.Name}");
+                Console.WriteLine($"Spesialitet: {shops.Speciality}");
+                Console.WriteLine($"Prisnivå: {shops.PriceRange}");
+                Console.WriteLine();
+            }
+        }
+
+        public void AddStore()
+        {
+            Console.WriteLine("Legg til dine favoritt butikker!");
+
+            Console.Write("Skriv navn: ");
+            var inputName = Console.ReadLine();
+
+            Console.Write("Skriv spesialitet: ");
+            var inputSpeciality = Console.ReadLine();
+
+            Console.Write("Skriv prisnivå: ");
+            var inputPriceRange = Console.ReadLine();
+
+
+        }
+    }
+}
